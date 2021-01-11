dc53e7ff755af0f2c206b81423a4365987452c1b
Vanessa <vanessainiguez98@gmail.com>
2021-01-11 00:07:49 -0500

    Add Navbar file component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import { FaRegMoon } from 'react-icons/fa';
+import React from 'react';
+
+import './Navbar.css';
+
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="text">Where in the world?</div>
+
+            <div className="dark-mode">
+                <FaRegMoon />
+                <div className="text-dark">Dark Mode</div>
+            </div>
+            
+        </div>
+    )
+}
